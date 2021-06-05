c524173a56f8b8b4cfed064a8be3c0701f5c3922
webdeveloper4hire <85345024+webdeveloper4hire@users.noreply.github.com>
2021-06-05 18:14:09 +0800

    Add files via upload

--- connections/connection.php
+++ connections/connection.php
@@ -5,6 +5,6 @@
 $hostname_connection = "localhost";
 $database_connection = "database";
 $username_connection = "root";
-$password_connection = "P@ssw0rd1";
+$password_connection = "";
 $connection = mysql_pconnect($hostname_connection, $username_connection, $password_connection) or trigger_error(mysql_error(),E_USER_ERROR); 
 ?>
